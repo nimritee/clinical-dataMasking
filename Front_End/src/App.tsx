c38fa557139c1fbdea04afe010dbcb2032ee099b
Nimritee <Nimritee1997@gmail.com>
2022-10-05 17:31:12 +0530

    apis added

--- Front_End/src/App.tsx
+++ Front_End/src/App.tsx
@@ -1,30 +1,67 @@
-import React from 'react';
-import './App.css';
-import { Box, Typography } from '@mui/material';
-import TextAreaWithTitle from './components/TextAreaWithTitle';
-import { MaskButton } from './components/MaskButton';
-import UploadFilesSection from './components/UploadFilesSection';
+import React, { useState } from "react";
+import "./App.css";
+import { Box, Typography } from "@mui/material";
+import TextAreaWithTitle from "./components/TextAreaWithTitle";
+import { MaskButton } from "./components/MaskButton";
+import UploadFilesSection from "./components/UploadFilesSection";
+import axios from "axios";
+var FormData = require('form-data');
+
+const BASE_URL = "http://127.0.0.1:5000/";
 
 function App() {
+  const [text, setText] = useState('')
+  const [outputText, setOutputText] = useState('')
+
+
+  async function getUser() {
+    try {
+      console.log(text)
+      var data = new FormData();
+      data.append(
+        "input_data",
+        text
+      );
+
+      const response = await axios.post(BASE_URL+"mask",data);
+      console.log(response.data);
+      setOutputText(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       {/* APP TITLE */}
       <Box mb={2}>
-        <Typography variant='h4' className='app-title'>Healthcare Data Masking</Typography>
+        <Typography variant="h4" className="app-title">
+          Healthcare Data Masking
+        </Typography>
       </Box>
 
       {/* TEXT AREAS SECTION */}
-      <Box display='flex' justifyContent={'space-between'} my={3}>
-        <TextAreaWithTitle title='Data to be Masked'/>
-        <TextAreaWithTitle title='Masked Data'/>
+      <Box display="flex" justifyContent={"space-between"} my={3}>
+        <TextAreaWithTitle title="Data to be Masked" text={text} setText={setText} disable={false} />
+        <TextAreaWithTitle title="Masked Data" text={outputText} disable={true} />
       </Box>
       <Box>
-        <MaskButton variant='contained' className='mask-btn'>MASK DATA</MaskButton>
+        <MaskButton variant="contained" className="mask-btn" onClick={getUser} >
+          MASK DATA
+        </MaskButton>
       </Box>
 
       {/* UPLOAD FILES SECTION */}
-      <Box display='flex' flexDirection={'column'} alignItems='flex-start' mt={4} ml={'4%'}>
-        <Typography variant='subtitle1'><b>Upload Files to be Masked</b></Typography>
+      <Box
+        display="flex"
+        flexDirection={"column"}
+        alignItems="flex-start"
+        mt={4}
+        ml={"4%"}
+      >
+        <Typography variant="subtitle1">
+          <b>Upload Files to be Masked</b>
+        </Typography>
         <UploadFilesSection />
       </Box>
     </div>
--- Front_End/src/components/UploadFilesSection/index.js
+++ Front_End/src/components/UploadFilesSection/index.js
@@ -1,48 +1,100 @@
-import { Box, Button, Typography } from '@mui/material'
-import React, {useCallback, useEffect, useState} from 'react'
-import {useDropzone} from 'react-dropzone'
+import { Box, Button, Typography } from "@mui/material";
+import React, { useCallback, useEffect, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
+var FormData = require('form-data');
 
 function UploadFilesSection() {
-  const {acceptedFiles, getRootProps, getInputProps, isDragActive} = useDropzone({maxFiles: 2})
-  const [uploadedFiles, setUploadedFiles] = useState([])
-  const files = uploadedFiles.map(file => (
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
+    useDropzone({ maxFiles: 2 });
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const files = uploadedFiles.map((file) => (
     <Box key={file.path}>
-        <Typography variant='body1' key={`${file.path}.name`}><b>File name:</b> {file.path}</Typography>
-        <Typography variant='body1' key={`${file.path}.size`}><b>Size:</b> {file.size} bytes</Typography>
+      <Typography variant="body1" key={`${file.path}.name`}>
+        <b>File name:</b> {file.path}
+      </Typography>
+      <Typography variant="body1" key={`${file.path}.size`}>
+        <b>Size:</b> {file.size} bytes
+      </Typography>
     </Box>
   ));
+  const BASE_URL = "http://127.0.0.1:5000/";
 
-  useEffect(()=>{
-    setUploadedFiles(acceptedFiles)
-  }, [acceptedFiles])
+  async function maskFiles(){
+    console.log(uploadedFiles)
 
-  const onResetClickHandler = useCallback(()=>{
-    setUploadedFiles([])
-  },[])
+    var data = new FormData();
+    // console.log("df",data.getHeaders())
+    for(let file of uploadedFiles){
+      data.append('file',
+      file );
+    }
+    const response = await axios.post(BASE_URL+"mask",data,{
+      responseType:"blob",
+      headers: {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    });
+
+    console.log(response.data);
+    var blob = new Blob([response.data], {
+        type: response.headers['content-type']
+    }); 
+    console.log(blob); 
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'output.zip'); //or any other extension
+    document.body.appendChild(link);
+    link.click();
+  }
+
+  useEffect(() => {
+    setUploadedFiles(acceptedFiles);
+  }, [acceptedFiles]);
+
+  const onResetClickHandler = useCallback(() => {
+    setUploadedFiles([]);
+  }, []);
 
   const { onClick } = getRootProps();
 
+ 
+
   return (
     <Box>
-        <Box {...getRootProps()} className='file-drop-section'>
-            <input {...getInputProps()} />
-            {
-                isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop files here, or click to select files</p>
-            }
+      <Box {...getRootProps()} className="file-drop-section">
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop files here, or click to select files</p>
+        )}
+      </Box>
+      {uploadedFiles.length > 0 && (
+        <Box pt={2} textAlign="left">
+          {files}
         </Box>
-        { uploadedFiles.length>0 && <Box pt={2} textAlign='left'>
-            {files}
-        </Box>}
-        <Box textAlign={'left'} mt={2} display='flex'>
-            <Box mr={2}>
-                <Button color='secondary' variant='contained' onClick={onClick}>Select Files</Button>
-            </Box>
-            <Button variant='contained' onClick={onResetClickHandler}>Reset</Button>
+      )}
+      <Box textAlign={"left"} mt={2} display="flex">
+        <Box mr={2}>
+          <Button color="secondary" variant="contained" onClick={onClick}>
+            Select Files
+          </Button>
         </Box>
+        <Button variant="contained" onClick={onResetClickHandler}>
+          Reset
+        </Button>
+        <Box  ml={2}>
+        <Button variant="contained" onClick={maskFiles}>
+          Mask files
+        </Button>
+        </Box>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default UploadFilesSection
+export default UploadFilesSection;
